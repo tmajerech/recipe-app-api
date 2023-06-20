528f120760a666be72ecc5758f58cc1eccaec40a
tmajerech <majerech.t@gmail.com>
2023-06-20 21:00:28 +0200

    added extend_schema_view to baserecipeattrview

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,17 +17,6 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
-class BaseRecipeAttrViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin,
-                            viewsets.GenericViewSet):
-    """Base viewset for recipe attributes."""
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-
 @extend_schema_view(
     list=extend_schema(
         parameters=[
@@ -94,6 +83,34 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description="Filter by items assigned to recipes."
+            )
+        ]
+    )
+)
+class BaseRecipeAttrViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base viewset for recipe attributes."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
